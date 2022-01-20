62a9019311b608f82827783cabc7302cdebe080b
Mahmoud-ms1996 <mahmoud.masri1996@gmail.com>
2022-01-20 22:00:12 +0200

    adding files

---
+++ js/main.js
@@ -0,0 +1,31 @@
+$(function(){
+    
+    'use strict';
+
+    $(window).scroll(function (){
+        var navbar = $('.navbar');
+
+        if ($ (window).scrollTop() >= navbar.height()){
+            navbar.addClass('scrolled');
+        } else{
+            navbar.removeClass('scrolled');
+        }
+
+        // Condition ? true : false;
+       // $ (window).scrollTop() >= navbar.height() ? navbar.addClass('scrolled') : navbar.removeClass('scrolled');
+    });
+
+    // Selected Tabs
+    $('.tab-switch li').click(function (){
+        
+        // Add Selected Class To Be Active Link
+        $(this).addClass('selected').siblings().removeClass('selected');
+        
+        //Hide All Divs
+        $('.tabs-section .tabs-content > div').hide();
+
+        // Show Div Connected With This link
+        $('.' + $(this).data('class')).show();
+    });
+
+});
